4de8c9c0c134f3e308a5f90f94f0f45ca2d6c6c7
Jul1zzZ <ruibastia@gmail.com>
2022-03-11 17:44:03 +0300

    wpfapps

---
+++ WpfApp1/WpfApp1/LoginWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<Authentication> authentications = FurnitureShopEntities.GetContext().Authentication.ToList();
+                Authentication au = authentications.FirstOrDefault(p => p.Password == TbPass.Password && p.Login == TbLogin.Text);
+                if (au != null)
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Owner = this;
+                    this.Hide();
+                    mainWindow.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Неверные данные");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult x = MessageBox.Show("Вы действительно хотите закрыть", "Выйти", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+            if (x == MessageBoxResult.Cancel)
+                e.Cancel = true;
+        }
+    }
+}
