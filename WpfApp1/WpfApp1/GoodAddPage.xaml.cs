04d5ead8d7854f204dc89a4945cecafb6e039d69
Jul1zzZ <ruibastia@gmail.com>
2022-03-17 19:56:18 +0300

    new

--- WpfApp1/WpfApp1/GoodAddPage.xaml.cs
+++ WpfApp1/WpfApp1/GoodAddPage.xaml.cs
@@ -35,9 +35,21 @@ namespace WpfApp1
                 _currentGood = selectedGoods;
                 int x = selectedGoods.ProductID;
                 List<Product> products = new List<Product>();
+                _filePath = _currentDirectory + _currentGood.Photo;
             }
+            DataContext = _currentGood;
+            _photoName = _currentGood.Photo; 
         }
 
+        private StringBuilder CheckFields()
+        {
+            StringBuilder s = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(_currentGood.ProductName)) s.AppendLine("Поле названия пустое");
+            if (_currentGood.SellPrice < 0) s.AppendLine("Цена не может быть отрицательной");
+            if (_currentGood.SellAmount < 0) s.AppendLine("Количество не может быть отрицательным");
+            if (string.IsNullOrWhiteSpace(_photoName)) s.AppendLine("Фото не выбрано");
+            return s;
+        }
         private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
         {
 
@@ -45,7 +57,79 @@ namespace WpfApp1
 
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
+            StringBuilder _error = CheckFields();
+            if (_error.Length > 0)
+            {
+                MessageBox.Show(_error.ToString());
+                return;
+            }
+            if (_currentGood.ProductID == 0)
+            {
+                string image = ChangePhotoName();
+                string dest = _currentDirectory + image;
+                File.Copy(_filePath, dest);
+                _currentGood.Photo = image;
+                FurnitureShopEntities.GetContext().Product.Add(_currentGood);
+            }
+            try
+            {
+                if (_filePath != null)
+                {
+                    string image = ChangePhotoName();
+                    string dest = _currentDirectory + image;
+                    File.Copy(_filePath, dest);
+                    _currentGood.Photo = image;
+                }
+                FurnitureShopEntities.GetContext().SaveChanges();
+                MessageBox.Show("Запись изменена");
+                Maneger.MainFrame.GoBack();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+        string ChangePhotoName()
+        {
+            string x = _currentDirectory + _photoName;
+            string photoname = _photoName;
+            int i = 0;
+            if (File.Exists(x))
+            {
+                while (File.Exists(x))
+                {
+                    i++;
+                    x = _currentDirectory + i.ToString() + photoname;
+                }
+                photoname = i.ToString() + photoname;
+            }
+            return photoname;
+        }
 
+        private void LoadImage_Click(object sender, RoutedEventArgs e)
+        {
+         try
+            {
+                OpenFileDialog op = new OpenFileDialog();
+                op.Title = "Select a picture";
+                op.Filter = "JPG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png";
+                if (op.ShowDialog() == true)
+                {
+                    FileInfo fileInfo = new FileInfo(op.FileName);
+                    if (fileInfo.Length > (2048 *2048 * 4))
+                    {
+                        throw new Exception("Размер файла должен быть меньше 4 Мб");
+                    }
+                    ImagePhoto.Source = new BitmapImage(new Uri(op.FileName));
+                    _photoName = op.SafeFileName;
+                    _filePath = op.FileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                _filePath = null;
+            }
         }
     }
 }
